6ff85a71e1d8ae20f8e6265545c096c8610c4b38
wangyuan <1918765432@qq.com>
2018-04-21 11:14:49 +0800

    init textss

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -8,5 +8,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("aa","aaassssss");
+
     }
 }
